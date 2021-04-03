c87baf2a0bd481a348ede1f85fe0b99f10020897
gabrieleng <gabe4937@gmail.com>
2021-04-03 11:04:55 -0700

    Removed jpg requirement

--- geglScript.sh
+++ geglScript.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-for filename in $(basename "./imgIn"/*.jpg .jpg)
+for filename in $(basename "./imgIn"/* )
 do
     echo "Processing: $filename" 
     for level in 0.2 0.4 0.6
     do
         echo "Adding $level noise-rgb to $filename"
-        gegl ./imgIn/${filename}.jpg -o ./imgOut/${filename}_guas_${level}.jpg -- noise-rgb gaussian=true red=${level} green=${level} blue=${level}
+        gegl ./imgIn/${filename} -o ./imgOut/${filename/./_}_guas_${level}.jpg -- noise-rgb gaussian=true red=${level} green=${level} blue=${level}
     done
     for level in 5 10 20
     do
         echo "Adding $level spread to $filename"
-        gegl ./imgIn/${filename}.jpg -o ./imgOut/${filename}_spread_${level}.jpg -- noise-spread amount-x=${level} amount-y=${level}
+        gegl ./imgIn/${filename} -o ./imgOut/${filename/./_}_spread_${level}.jpg -- noise-spread amount-x=${level} amount-y=${level}
     done
 done
